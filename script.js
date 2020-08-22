24a99a9fd0f3164d87bbfad586abbfe85bbe5d06
David Gibbons <gibbonsdav@gmail.com>
2020-08-21 20:53:43 -0700

    getRandomUser

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ const doubleBtn = document.getElementById("double")
 const showMillionairesBtn = document.getElementById("show-millionaires")
 const sortBtn = document.getElementById("sort")
 const calculateWeathBtn = document.getElementById("calculate-wealth")
+
+let data = []
+
+getRandomUser()
+getRandomUser()
+getRandomUser()
+
+// fetch random user and add wealth
+
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api")
+  const data = await res.json()
+
+  const user = data.results[0]
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  }
+
+  addData(newUser)
+}
+
+//add new obj to data arr
+function addData(obj) {
+  data.push(obj)
+}
