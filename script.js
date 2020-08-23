ce9cf49c1211625b58d8637b52113abe0bfc50df
David Gibbons <gibbonsdav@gmail.com>
2020-08-22 19:43:44 -0700

    JS

--- script.js
+++ script.js
@@ -30,4 +30,26 @@ async function getRandomUser() {
 //add new obj to data arr
 function addData(obj) {
   data.push(obj)
+
+  updateDOM()
+}
+
+//update DOM
+function updateDOM(providedData = data) {
+  //clear main div
+  main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>"
+
+  providedData.forEach((item) => {
+    const element = document.createElement("div")
+    element.classList.add("person")
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`
+    main.appendChild(element)
+  })
+}
+
+//format number as money
+function formatMoney(number) {
+  return "$" + number.toFixed(2)
 }
